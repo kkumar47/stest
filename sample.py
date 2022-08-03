ab1fb0616611a3b04ba1f554dd326da03eef57d5
kkumar47 <kkeee2k4@gmail.com>
2022-08-03 15:04:21 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -18,7 +18,7 @@ def raw_data():
 	token = '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'
 	r = requests.get('https://api.github.com/repos/{owner}/{repo}/contents/{path}'.format(owner=owner, repo=repo, path=path),headers={'accept': 'application/vnd.github.v3.raw','authorization': 'token {}'.format(token)}, verify=False)
 	string_io_obj = StringIO(r.text)
-	return pd.read_csv(string_io_obj, usecols=["Meter","Date_Raw","Usage"])
+	return pd.read_csv(string_io_obj)
 rawdf = raw_data()
 
 with loaddata:
