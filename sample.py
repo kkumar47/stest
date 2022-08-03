8e8c82d0e6e19f9a380eccc24d7d10d9a3e1394a
kkumar47 <kkeee2k4@gmail.com>
2022-08-03 15:26:16 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,9 +2,14 @@ import streamlit as st
 import pandas as pd
 import requests
 from io import StringIO
+import numpy as np
+import datetime as dt
+import calendar
+import random
 
 mainc = st.container()
 loaddata = st.container()
+pprocess = st.container()
 
 with mainc:
 	font="sans serif"
@@ -18,8 +23,8 @@ def good_data():
 def bad_data():
 	return pd.read_csv('https://raw.githubusercontent.com/kkumar47/Usage-Data/main/Final/Final_Bad_Customer.csv')
 
-goodd = good_data()
-badd = bad_data()
+Good_Residential = good_data()
+Bad_Residential = bad_data()
 
 with loaddata:
 	st.subheader("Electricity Usage History data for Customers", anchor ='The Data')
@@ -27,9 +32,36 @@ with loaddata:
 	with col3:
 		st.text('Raw Data - Good Customer')
 		#The raw data is displayed here
-		st.dataframe(goodd.head(10))
+		st.dataframe(Good_Residential.head(10))
 	with col4:
 		st.text('Raw Data - Bad Customer')
-		st.dataframe(badd.head(10))
+		st.dataframe(Bad_Residential.head(10))
 		
+		
+with pprocess:
+	st.subheader("Pre-Process Data")
+	#Process Good Customer Data
+	Good_Residential['Hr'] = (((Good_Residential['Date_Raw']-1)%100)*30)//60
+	Good_Residential['Day'] = abs(Good_Residential['Date_Raw']//100)
+	Good_Residential = Good_Residential.drop(columns=['Date_Raw'])
+	Good_Residential['Date'] = Good_Residential['Day'].apply(lambda x: pd.to_datetime((2009*1000 )+ x, format = "%Y%j") if x<=365 else pd.to_datetime((2010*1000 )+ (x-365), format = "%Y%j"))
+	Good_Residential['Day_Num'] = Good_Residential['Date'].apply(lambda x: x.weekday())
+	Good_Residential['Dayname'] = Good_Residential['Date'].apply(lambda x: calendar.day_name[x.weekday()])
+	Good_Residential['Holiday_Ind'] = Good_Residential['Day_Num'].apply(lambda x: 0 if x<=4 else 1)
+	Good_Residential['Month'] = Good_Residential['Date'].apply(lambda x: x.strftime("%B"))
+	Good_Residential['Year'] = Good_Residential['Date'].apply(lambda x: x.year)
+	def condition(x):
+ 		if (x=='August' or x=='September' or x=='October'):
+    			return "Autumn"
+  		elif (x=='November' or x=='December' or x=='January'):
+    			return "Winter"
+  		elif (x=='February' or x=='March' or x=='April'):
+    			return "Spring"
+  		elif (x=='May' or x=='June' or x=='July'):
+    			return "Summer"
+	Good_Residential['Season']=Good_Residential['Month'].apply(condition)
+	col5, col6 = st.columns(2)
+	col5.dataframe(Good_Residential)
+	
+	
 	
