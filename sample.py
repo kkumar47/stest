b1da702d13ba9b09e8317b1b7094dddb3db0b91e
kkumar47 <kkeee2k4@gmail.com>
2022-08-03 15:11:08 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -11,25 +11,25 @@ with mainc:
 	textColor="#26273"
 	st.title('Electricity Theft Prediction')
 	
-def raw_data():
-	repo ='Private_Data'
-	path='Raw.txt'
-	owner = 'kkumar47'
-	token = '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'
-	r = requests.get('https://api.github.com/repos/{owner}/{repo}/contents/{path}'.format(owner=owner, repo=repo, path=path),headers={'accept': 'application/vnd.github.v3.raw','authorization': 'token {}'.format(token)}, verify=False)
-	string_io_obj = StringIO(r.text)
-	return pd.read_csv(string_io_obj)
-rawdf = raw_data()
+
 def good_data():
-	return pd.read_csv('https://raw.githubusercontent.com/kkumar47/Usage-Data/main/Final/Good_Residential.csv')
+	return pd.read_csv('https://raw.githubusercontent.com/kkumar47/Usage-Data/main/Final/Good_Residential.csv')[['Meter', 'Date_Raw','Usage']]
+
+def bad_data():
+	return pd.read_csv('https://raw.githubusercontent.com/kkumar47/Usage-Data/main/Final/Final_Bad_Customer.csv')
 
 goodd = good_data()
+badd = bad_data()
 
 with loaddata:
 	st.subheader("Electricity Usage History data for Customers", anchor ='The Data')
 	col3,col4 = st.columns(2)
 	with col3:
-		st.text('Raw Data')
+		st.text('Raw Data - Good Customer')
 		#The raw data is displayed here
 		st.dataframe(goodd.head(10))
+	with col4:
+		st.text('Raw Data - Bad Customer')
+		st.dataframe(badd.head(10))
+		
 	
